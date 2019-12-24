f1bfab1603b3e9c364c131f7134500a62dbade21
A.J. Ruckman <aj@ruckman.dev>
2019-12-24 10:32:46 -0500

    add go.mod; add Printfln() function

--- print.go
+++ print.go
@@ -1,11 +1,15 @@
 package xlib
 
 import (
-	"fmt"
-	"strings"
+    "fmt"
+    "strings"
 )
 
 func PrintIndent(indent int, v ...interface{}) {
-	fmt.Print(strings.Repeat("\t", indent))
-	fmt.Println(v...)
+    fmt.Print(strings.Repeat("\t", indent))
+    fmt.Println(v...)
+}
+
+func Printfln(format string, a ...interface{}) {
+    fmt.Printf(format+"\n", a)
 }
